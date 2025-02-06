55d10385a5e237f72fdd129761b188f8014453c9
Mutairu-Lawal <thebillionnairadream@gmail.com>
2025-02-06 11:47:22 +0000

    update code with typescript

--- cal.js
+++ cal.js
@@ -1,96 +1,88 @@
-let display = document.querySelector(".showDisplay");
-const switchBtn = document.querySelector(".js-onBtn");
-
-let calculation = "";
-
+'use strict';
+var _a;
+let display = document.querySelector('.showDisplay');
+const switchBtn = document.querySelector('.js-onBtn');
+let calculation = '';
 renderCalculation();
-
 function getCalculation(value) {
-  if (value === "00" && calculation === "0") {
+  if (value === '00' && calculation === '0') {
     return;
-  } else if (value === "00" && calculation === "") {
+  } else if (value === '00' && calculation === '') {
     return;
-  } else if (value === "0" && calculation === "0") {
+  } else if (value === '0' && calculation === '0') {
     return;
-  } else if (value === "0" && calculation + value === "00") {
+  } else if (value === '0' && calculation + value === '00') {
     return;
   }
-
   calculation += value;
   checkZero();
   checkOperators(value);
   renderCalculation();
 }
-
 function checkOperators(value) {
   if (
-    value === "*" ||
-    value === "+" ||
-    value === "-" ||
-    value === "/" ||
-    value === "."
+    value === '*' ||
+    value === '+' ||
+    value === '-' ||
+    value === '/' ||
+    value === '.'
   ) {
     if (calculation.includes(`${value + value}`)) {
       calculation = removeLastIndex(calculation);
     }
   }
 }
-
 function updateCalculation() {
   try {
-    calculation = eval(display.value);
-    filterResult();
-    renderCalculation();
-    console.log(typeof calculation);
+    if (display) {
+      calculation = eval(display.value);
+      filterResult();
+      renderCalculation();
+    }
   } catch (error) {
-    renderCalculation(error);
+    renderCalculation();
   }
 }
-
 function resetBtn() {
-  calculation = "";
+  calculation = '';
   renderCalculation();
 }
-
 function renderCalculation() {
-  display.value = calculation;
+  if (display) {
+    display.value = calculation;
+  }
 }
-
 function checkZero() {
-  if (calculation.includes(".")) {
+  if (calculation.includes('.')) {
     calculation = calculation;
     return;
   } else if (calculation) {
     for (let i = 0; i < calculation.length; i++) {
-      index = calculation[i];
-      if (index !== "0") {
+      let index = calculation[i];
+      if (index !== '0') {
         calculation = calculation.slice(i);
         break;
       }
     }
   }
 }
-
 function filterResult() {
   calculation = String(calculation);
   if (calculation.length > 8) {
-    calculation = Number(calculation);
-    calculation = calculation.toFixed(2);
-    calculation = Number(calculation);
+    calculation = Number(calculation).toFixed(2);
   }
 }
-
 function removeLastIndex(input) {
-  let newString = [...input].slice(0, -1).join("");
+  let newString = [...input].slice(0, -1).join('');
   return newString;
 }
-
-document.querySelector(".js-delete-button").addEventListener("click", () => {
-  calculation = removeLastIndex(calculation);
-  renderCalculation();
-});
-
+(_a = document.querySelector('.js-delete-button')) === null || _a === void 0
+  ? void 0
+  : _a.addEventListener('click', () => {
+      calculation = removeLastIndex(calculation);
+      renderCalculation();
+    });
 function percentageBtn() {
-  calculation = calculation / 100;
+  calculation = String(Number(calculation) / 100);
   renderCalculation();
 }
